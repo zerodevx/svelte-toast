34ec81b8fc84cf14a062c88b93e76a8eef9f8d8a
Jason Lee <jason@zerodevx.com>
2020-09-15 17:11:26 +0800

    Add lint configs

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  plugins: [
+    'svelte3'
+  ],
+  overrides: [
+    {
+      files: ['**/*.svelte'],
+      processor: 'svelte3/svelte3',
+      rules: {
+        'import/first': 0,
+        'import/no-duplicates': 0,
+        'import/no-mutable-exports': 0,
+        'no-multiple-empty-lines': 0
+      }
+    }
+  ]
+}
