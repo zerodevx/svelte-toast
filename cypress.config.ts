b3d008655ab7546df62087962fc311d4bbc6eed1
Jason Lee <jason@zerodevx.com>
2022-11-28 16:20:36 +0800

    Big migration to `svelte-package`

--- cypress.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'cypress'
-
-export default defineConfig({
-  screenshotOnRunFailure: false,
-  video: false,
-  e2e: {
-    baseUrl: 'http://localhost:3000'
-  }
-})
---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-static'
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter()
+  },
+  preprocess: [
+    preprocess({
+      postcss: true
+    })
+  ]
+}
+
+export default config
