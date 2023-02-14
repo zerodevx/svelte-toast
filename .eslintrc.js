1e9057789774de85c520e9cdc531ca503cb2b102
Jason Lee <jason@zerodevx.com>
2023-02-14 17:42:39 +0800

    Upgrade tooling

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,19 @@
 module.exports = {
   root: true,
   extends: ['eslint:recommended', 'prettier'],
-  overrides: [
-    { files: ['*.svelte'], plugins: ['svelte3'], processor: 'svelte3/svelte3' },
-    { files: ['*.html'], plugins: ['html'] },
-    {
-      files: ['*.ts'],
-      parser: '@typescript-eslint/parser',
-      plugins: ['@typescript-eslint'],
-      extends: ['plugin:@typescript-eslint/recommended'],
-      rules: { '@typescript-eslint/no-explicit-any': 'off' }
-    }
-  ],
+  plugins: ['svelte3'],
+  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 2019
+    ecmaVersion: 2020
   },
   env: {
     browser: true,
     es2017: true,
     node: true
+  },
+  rules: {
+    'no-tabs': 'error',
+    'no-unexpected-multiline': 'error'
   }
 }
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+const dt = require('tailwindcss/defaultTheme')
+
+const config = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['InterVariable', ...dt.fontFamily.sans]
+      }
+    }
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: ['light', 'dark']
+  }
+}
+
+module.exports = config
