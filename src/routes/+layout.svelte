681fa421818427ca54002d6893da69986ab4eec9
Jason Lee <jason@zerodevx.com>
2023-02-20 23:40:33 +0800

    Migrate demo to `svelte-kit`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,22 @@
 <script>
+import '@fontsource/inter/variable.css'
+import 'prismjs/themes/prism-tomorrow.css'
 import '../app.postcss'
+import { dev } from '$app/environment'
 </script>
+
+<svelte:head>
+  {#if !dev}
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-G9JC5N7N1H"></script>
+  {/if}
+  <script>
+  window.dataLayer = window.dataLayer || []
+  function gtag() {
+    dataLayer.push(arguments)
+  }
+  gtag('js', new Date())
+  gtag('config', 'G-G9JC5N7N1H')
+  </script>
+</svelte:head>
 
 <slot />
