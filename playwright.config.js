6d6ac3347f5d7142a707e5b64e02feb3a2447f71
Jason Lee <jason@zerodevx.com>
2023-03-25 23:55:00 +0800

    Fix dual dev/prod test runner

--- playwright.config.js
+++ playwright.config.js
@@ -5,9 +5,12 @@ const dev = await check(5173, { host: 'localhost' })
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
   webServer: {
-    command: 'npm run build && npm run preview',
+    command: 'npx vite build && npx vite preview',
     port: dev ? 5173 : 4173,
     reuseExistingServer: dev
+  },
+  use: {
+    baseURL: `http://localhost:${dev ? '5173' : '4173/svelte-toast/'}`
   },
   testDir: 'tests'
 }
