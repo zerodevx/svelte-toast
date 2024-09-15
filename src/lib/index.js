475e42e2e83a4be666b5ac837af71aae97f82113
Ollie <dannyworks102@gmail.com>
2024-09-15 18:12:51 +0900

    Expose `SvelteToastOptions` (#107)

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,6 @@
 export { default as SvelteToast } from './SvelteToast.svelte'
 export { toast } from './stores.js'
+
+/**
+ * @typedef {import('./stores.js').SvelteToastOptions} SvelteToastOptions
+ */
